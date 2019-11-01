31610536a426b5deb2c7582af58bb99d1d2da209
gunturnawaksara <54344876+gunturnawaksara@users.noreply.github.com>
2019-11-01 23:19:23 +0700

    Update design

--- EatKuyProjects/src/main/java/fxml/ProfilController.java
+++ EatKuyProjects/src/main/java/fxml/ProfilController.java
@@ -5,14 +5,20 @@
  */
 package fxml;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -60,22 +66,34 @@ public class ProfilController implements Initializable {
 
     @FXML
     private void profileButton(ActionEvent event) {
+        String navProfile="Profile";
+        this.navigasi.setText(navProfile);
     }
 
     @FXML
     private void breakfastButton(ActionEvent event) {
+        String navBreakfast="Breakfast";
+        this.navigasi.setText(navBreakfast);
     }
 
     @FXML
     private void lunchButton(ActionEvent event) {
+        String navLunch="Lunch";
+        this.navigasi.setText(navLunch);
     }
 
     @FXML
     private void dinnerButton(ActionEvent event) {
+        String navDinner="Dinner";
+        this.navigasi.setText(navDinner);
     }
 
     @FXML
-    private void logoutButton(ActionEvent event) {
+    private void logoutButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.setScene(new Scene(root));
     }
 
     @FXML
