dc25942a8a3847c66d702a780f5c7aac0a2145cc
Kukuh Nugroho <kukuh.nugroho@ti.ukdw.ac.id>
2019-11-14 01:18:36 +0700

    Update History

--- EatKuyProjects/src/main/java/fxml/HomeController.java
+++ EatKuyProjects/src/main/java/fxml/HomeController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
@@ -101,6 +102,8 @@ public class HomeController implements Initializable {
     private TextField makananTxt;
     @FXML
     private TextField kaloriTxt;
+    @FXML
+    private ImageView tambahBtn;
 
     /**
      * Initializes the controller class.
@@ -262,20 +265,24 @@ public class HomeController implements Initializable {
         }
     }
     
-    public void loadMakananDB(){
-        loadDB2(col_idMakanan, col_namaMakanan, col_KaloriMakanan, makananTabel);
-    }
-    
-    public void loadCatatDB(){
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        db = new DBUtil();
+        comboJK.setItems(JK);
+        comboAktivitas.setItems(aktivitas);
+        comboSesi.setItems(sesi);
         loadDB1(col_idCatat, col_namaMakananCatat, col_kaloriCatat, col_sesiCatat, catatMakanan);
-    }
-    
-    public void searchFiltered(TextField session, TableView<Makanan> tabSeacrh){
+        loadDB2(col_idMakanan, col_namaMakanan, col_KaloriMakanan, makananTabel);
+    } 
+
+    @FXML
+    private void filterMakanan(ActionEvent event) {
         ObservableList<Makanan> data;
         try {
             data = MakananDAO.searchMakanans();
             final FilteredList<Makanan> filteredList = new FilteredList<>(data);
-            session.textProperty().addListener(new ChangeListener<String>() {
+            searchField.textProperty().addListener(new ChangeListener<String>() {
                 @Override
                 public void changed(ObservableValue<? extends String> observable, String oldValue, final String newValue) {
                     filteredList.setPredicate(new Predicate<Makanan>() {
@@ -296,48 +303,24 @@ public class HomeController implements Initializable {
                 }
             });
             SortedList<Makanan> sortedData = new SortedList<>(filteredList);
-            sortedData.comparatorProperty().bind(tabSeacrh.comparatorProperty());
-            tabSeacrh.setItems(sortedData);
+            sortedData.comparatorProperty().bind(makananTabel.comparatorProperty());
+            makananTabel.setItems(sortedData);
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(MakananManajemenController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        db = new DBUtil();
-        comboJK.setItems(JK);
-        comboAktivitas.setItems(aktivitas);
-        comboSesi.setItems(sesi);
-        loadMakananDB();
-        loadCatatDB();
-        try {
-            pindahHistory();
-        } catch (SQLException ex) {
-            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    } 
-
-    @FXML
-    private void filterMakanan(ActionEvent event) {
-        searchFiltered(searchField, makananTabel);
     }
 
     @FXML
     private void historyButton(ActionEvent event) throws IOException {
-         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/History.fxml"));
-                    Parent Main = (Parent) loader.load();
-                    HistoryController his = (HistoryController)loader.getController();
-                    his.GetUserLogin(this.sessionUsername);
-                    Scene scene = new Scene(Main);
-                    Stage Primarystage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-                    Primarystage.setResizable(false);
-                    Primarystage.setScene(scene);
-                    Primarystage.show();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/History.fxml"));
+        Parent Main = (Parent) loader.load();
+        HistoryController his = (HistoryController)loader.getController();
+        his.GetUserLogin(this.sessionUsername);
+        Scene scene = new Scene(Main);
+        Stage Primarystage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Primarystage.setResizable(false);
+        Primarystage.setScene(scene);
+        Primarystage.show();
     }  
 
     @FXML
@@ -361,11 +344,12 @@ public class HomeController implements Initializable {
         String namaM = makananTxt.getText();
         String kaloriM = kaloriTxt.getText();
         String sesi = comboSesi.getValue();
-        String pattern = "yyyy-mm-dd";
+        String pattern = "yyyy-MM-dd";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
         String date = simpleDateFormat.format(new Date());
-        try{
-                String query = "INSERT INTO MakananHarian(NamaMakanan, Kalori, SesiMakan, Tanggal) VALUES('"+namaM+"','"+kaloriM+"','"+sesi+"','"+date+"')";
+        if(sesi != null){
+            try{
+                String query = "INSERT INTO MakananHarian(NamaMakanan, Kalori, SesiMakan, Tanggal, Username) VALUES('"+namaM+"','"+kaloriM+"','"+sesi+"','"+date+"','"+sessionUsername+"')";
                 db.dbExecuteUpdate(query);
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("ADD FOOD SUCCESS");
@@ -374,26 +358,28 @@ public class HomeController implements Initializable {
                 alert.showAndWait();
                 clearField();
                 loadDB1(col_idCatat, col_namaMakananCatat, col_kaloriCatat, col_sesiCatat, catatMakanan);
-        }catch(Exception e){
-            System.out.print(e);
+            }catch(Exception e){
+                System.out.print(e);
+            }
         }
     }
     
     public void pindahHistory() throws SQLException, ClassNotFoundException{
-        String pattern = "yyyy-mm-dd";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-        String date = simpleDateFormat.format(new Date());
-        System.out.print(date);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
         String query = "SELECT * FROM MakananHarian WHERE Username= '"+sessionUsername+"'";
-        ResultSet rs2 = db.dbExecuteQuery(query);
-        while(rs2.next()){
-            if(String.valueOf(rs2.getDate("Tanggal")) != date){
-                String queryInsert = "INSERT INTO MakananHistory(ID_History, User, NamaMakanan, Kalori, Tanggal VALUES('"+rs2.getString("ID_catat")+"','"+sessionUsername+"','"+rs2.getString("NamaMakanan")+"','"+rs2.getString("Kalori")+"','"+rs2.getString("Tanggal")+"')";
-                db.dbExecuteUpdate(queryInsert);
+        ResultSet rs = db.dbExecuteQuery(query);
+        if(rs.next()){
+            while(rs.next()){
+                if(rs.getString("Tanggal") != dateFormat.format(date)){
+                    String queryInsert = "INSERT INTO MakananHistory(ID_History, User, NamaMakanan, Kalori, Tanggal) VALUES('"+rs.getString("ID_catat")+"','"+sessionUsername+"','"+rs.getString("NamaMakanan")+"','"+rs.getString("Kalori")+"','"+rs.getString("Tanggal")+"')";
+                    db.dbExecuteUpdate(queryInsert);
+                    String queryReset = "DELETE FROM MakananHarian WHERE NamaMakanan = '"+rs.getString("NamaMakanan")+"'";
+                    db.dbExecuteUpdate(queryReset);
+                } 
             }
+            loadDB1(col_idCatat, col_namaMakananCatat, col_kaloriCatat, col_sesiCatat, catatMakanan);
         }
-        String queryReset = "DELETE FROM MakananHarian WHERE 1";
-        db.dbExecuteUpdate(queryReset);
     }
-
+    
 }
